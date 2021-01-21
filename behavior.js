a5c828b59d81d6064779432429f59c4e7af7d2f8
sunny <sunny@sunny.com>
2021-01-21 17:58:20 +0200

    Added menu btn. Updated a bunch of css styles

--- behavior.js
+++ behavior.js
@@ -3,6 +3,7 @@
 import { handleContent } from "./js/content.js";
 
 let nav = document.body.querySelector(".nav-container"),
+    menuBtn = document.body.querySelector(".menu-wrap"),
     footerMore = document.body.querySelectorAll(".footer-more"),
     newsletter = document.body.querySelector("#newsletter");
     
@@ -13,9 +14,10 @@ window.addEventListener("resize", handleNav);
 window.addEventListener("resize", handleFooter);
 document.addEventListener("scroll", handleIsNav);
 document.addEventListener("scroll", handleIsImg);
+menuBtn.addEventListener("click", handleMenu);
 footerMore[0].parentElement.addEventListener("click", handleFooterMore);
-newsletter.addEventListener("mouseenter", handleShowNewsletter);
-newsletter.addEventListener("mouseleave", handleHideNewsletter);
+// newsletter.addEventListener("mouseenter", handleShowNewsletter);
+// newsletter.addEventListener("mouseleave", handleHideNewsletter);
 
 function handleWindowLoad() {
     handleIsImg();
@@ -36,16 +38,15 @@ function handlePageContent() {
 
 
 function handleNav() {
-    let menuBtn = document.body.querySelector("#menu-btn");
     if (window.innerWidth > 768) {
         nav.classList.remove("passive");
-        menuBtn.classList.add("passive");
-        newsletter.classList.remove("passive");
+        // menuBtn.classList.add("passive");
+        // newsletter.classList.remove("passive");
     }
     if (window.innerWidth < 768) {
         nav.classList.add("passive");
-        newsletter.classList.add("passive");
-        menuBtn.classList.remove("passive");
+        // newsletter.classList.add("passive");
+        // menuBtn.classList.remove("passive");
     }
 }
 
@@ -115,6 +116,14 @@ function handleIsImg() {
     }
 }
 
+function handleMenu() {
+    let menu = document.body.querySelector(".menu-nav-container");
+    menu.classList.toggle("menu-active");
+    menu.classList.contains("menu-active") ? 
+        document.body.style.overflow = "hidden" : 
+        document.body.style.overflow = ""; 
+}
+
 function handleFooterMore(e) {
     let elem = e.target.closest("a");
     if (elem === null || elem.closest(".footer-title") === null || window.innerWidth > 768) return;
@@ -127,79 +136,79 @@ function handleFooterMore(e) {
     window.scrollBy(0, 300);
 }
 
-function handleShowNewsletter() {
-    let tooltip = document.createElement("div");
-    tooltip.setAttribute("id", "tooltip");
-    tooltip.innerHTML = `
-    <div class="newsletter-tooltip-container">
-        <div class="" id="newsletter-tooltip-title">Užsiregistruokite Mūsų Naujienlaiškiui</div>
-        <div class="input-container">
-            <form>
-                <input type="email" id="tooltip-email-input" placeholder=" Jūsų el. pašto adresas">
-                <input type="submit" id="tooltip-email-submit" value="Registruotis">
-            </form>
-        </div>
-    </div>`;
-    tooltip.addEventListener("mouseenter", handleTooltipMouseOver);
-    tooltip.addEventListener("mouseleave", handleTooltipMouseOut);
+// function handleShowNewsletter() {
+//     let tooltip = document.createElement("div");
+//     tooltip.setAttribute("id", "tooltip");
+//     tooltip.innerHTML = `
+//     <div class="newsletter-tooltip-container">
+//         <div class="" id="newsletter-tooltip-title">Užsiregistruokite Mūsų Naujienlaiškiui</div>
+//         <div class="input-container">
+//             <form>
+//                 <input type="email" id="tooltip-email-input" placeholder=" Jūsų el. pašto adresas">
+//                 <input type="submit" id="tooltip-email-submit" value="Registruotis">
+//             </form>
+//         </div>
+//     </div>`;
+//     tooltip.addEventListener("mouseenter", handleTooltipMouseOver);
+//     tooltip.addEventListener("mouseleave", handleTooltipMouseOut);
 
-    document.body.append(tooltip);
+//     document.body.append(tooltip);
 
-    let submit = document.body.querySelector("#tooltip-email-submit").parentElement;
-    submit.addEventListener("submit", handleSubmit);
+//     let submit = document.body.querySelector("#tooltip-email-submit").parentElement;
+//     submit.addEventListener("submit", handleSubmit);
 
     
-    tooltip.style.position = "fixed";
-    tooltip.style.top = "15%";
-    tooltip.style.left = window.innerWidth - tooltip.offsetWidth - 25 + "px";
+//     tooltip.style.position = "fixed";
+//     tooltip.style.top = "15%";
+//     tooltip.style.left = window.innerWidth - tooltip.offsetWidth - 25 + "px";
 
-    function handleTooltipMouseOver() {
-        clearTimeout(handleHideNewsletter.timer);
-    }
+//     function handleTooltipMouseOver() {
+//         clearTimeout(handleHideNewsletter.timer);
+//     }
 
-    function handleTooltipMouseOut() {
-        handleHideNewsletter("", 1000);
-    }
+//     function handleTooltipMouseOut() {
+//         handleHideNewsletter("", 1000);
+//     }
     
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log('e.target.querySelector("#tooltip-email-input")', 
-        e.target.querySelector("#tooltip-email-input").value)
-        if (e.target.querySelector("#tooltip-email-input").value === "") return;
-        tooltip.innerHTML = `<div class="newsletter-tooltip-container">
-        <div class="" id="newsletter-tooltip-title">Sėkmingai užsiregistravote!</div>`;
+//     function handleSubmit(e) {
+//         e.preventDefault();
+//         console.log('e.target.querySelector("#tooltip-email-input")', 
+//         e.target.querySelector("#tooltip-email-input").value)
+//         if (e.target.querySelector("#tooltip-email-input").value === "") return;
+//         tooltip.innerHTML = `<div class="newsletter-tooltip-container">
+//         <div class="" id="newsletter-tooltip-title">Sėkmingai užsiregistravote!</div>`;
         
-    }
-}
+//     }
+// }
 
-function handleHideNewsletter(e, time = 500) {
-    if (document.body.querySelector("#tooltip")) {
-        handleHideNewsletter.timer = setTimeout(() => document.body.querySelector("#tooltip").remove(), time);
-    }
-}
+// function handleHideNewsletter(e, time = 500) {
+//     if (document.body.querySelector("#tooltip")) {
+//         handleHideNewsletter.timer = setTimeout(() => document.body.querySelector("#tooltip").remove(), time);
+//     }
+// }
 
-function handleNavBtn() {
-    let div = document.createElement("div"),
-        stilius = document.querySelector("#stilius").cloneNode(true),
-        kultura = document.querySelector("#kultura"),
-        spinta = document.querySelector("#spinta"),
-        dovanu_idejos = document.querySelector("#dovanu_idejos");
-        console.log('dovanu_idejos', dovanu_idejos)
-    div.style.width = "100%";
-    div.style.height = "100%";
-    div.style.position = "fixed";
-    div.style.marginTop = document.body.querySelector(".title-container").getBoundingClientRect().bottom + 20;
-    div.style.backgroundColor = "white";
-    div.style.top = -window.innerHeight + "px";
-    div.style.transition = "top 0s";
-    setTimeout(() => div.style.top = 0 + "px");
-    div.append(stilius);
-    div.append(kultura);
-    div.append(spinta);
-    div.append(dovanu_idejos);
-    document.body.append(div);
+// function handleNavBtn() {
+//     let div = document.createElement("div"),
+//         stilius = document.querySelector("#stilius").cloneNode(true),
+//         kultura = document.querySelector("#kultura"),
+//         spinta = document.querySelector("#spinta"),
+//         dovanu_idejos = document.querySelector("#dovanu_idejos");
+//         console.log('dovanu_idejos', dovanu_idejos)
+//     div.style.width = "100%";
+//     div.style.height = "100%";
+//     div.style.position = "fixed";
+//     div.style.marginTop = document.body.querySelector(".title-container").getBoundingClientRect().bottom + 20;
+//     div.style.backgroundColor = "white";
+//     div.style.top = -window.innerHeight + "px";
+//     div.style.transition = "top 0s";
+//     setTimeout(() => div.style.top = 0 + "px");
+//     div.append(stilius);
+//     div.append(kultura);
+//     div.append(spinta);
+//     div.append(dovanu_idejos);
+//     document.body.append(div);
 
-    function styler(elem) {
-        return `<div class="nav-container">` + elem + `</div>`;
-    }
-}
+//     function styler(elem) {
+//         return `<div class="nav-container">` + elem + `</div>`;
+//     }
+// }
